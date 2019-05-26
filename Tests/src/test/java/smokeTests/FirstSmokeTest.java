5a683ba19273b54033e54bd5291fdac68a173483
learningacc2 <test@example.org>
2019-05-26 13:11:11 +0200

    commitacc2

--- Tests/src/test/java/smokeTests/FirstSmokeTest.java
+++ Tests/src/test/java/smokeTests/FirstSmokeTest.java
@@ -16,6 +16,6 @@ public class FirstSmokeTest {
                 .clickCarsSearchButton();
         CarsPage carsPage = new CarsPage();
         carsPage.selectCarsRandomly();
-
+        carsPage.selectCarsRandomly();
     }
 }
